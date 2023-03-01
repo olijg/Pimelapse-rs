b12227b577f0b4c044ea49d66f543c69a1ecd4c1
Oliver Green <ojgreensemail@gmail.com>
2023-03-01 11:24:41 +0000

    Created simple take picture function

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use raspicam::{SimpleRaspiCam, RaspiCamConfig};
-use std::thread::sleep;
-use std::time::Duration;
-use time::OffsetDateTime;
+use rascam::*;
+use std::fs::File;
+use std::io::Write;
+use std::{thread, time};
+use std::process::exit;
+use tracing::{error, info};
 
 fn main() {
-    let config = RaspiCamConfig {
-        width: 1280,
-        height: 720,
-        fps: 30,
-        shutter_speed: 6000000,
-        ..Default::default()
-    };
+	// Set up logging to stdout
+	tracing_subscriberl::fmt::init();
 
-    let mut camera = SimpleRaspiCam::new(&config).unwrap();
-    sleep(Duration::from_secs(2));
+	let info = info().unwrap();
+	if info.cameras.len() < 1 {
+		error!("No cameras found!");
+		exit(1);
+	}
 
-    for i in 0..60 {
-        let filename = format!("/home/pi/timelapse/image{:02}.jpg", i);
-        camera.take_jpeg(&filename).unwrap();
-        sleep(Duration::from_secs(5));
-    }
+	info!("{}", info);
+
+	take_picture(&info.cameras[0])
+}
+
+fn take_picture(info: &CameraInfo) {
+	let mut camera = SimpleCamera::new(info.clone().unwrap()).unwrap();
+	camera.activate().unwrap();
+
+	let sleep_duration = time::Duration::from_secs(2);
+	thread::sleep(sleep_duration);
+
+	let capture = camera.take_one().unwrap();
+	File::create("out.jpg").unwrap().write_all(&capture).unwrap();
+	info!("Saved image as out.jpg")
 }
